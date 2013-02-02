38545bbde7f119509eef7412839e5ad6251823ac
Andrew Kalek <andrew.kalek@anlek.com>
2013-02-02 10:43:43 -0500

    Removed test folder

--- test/unit/calendar_date_test.rb
+++
@@ -1,7 +0,0 @@
-require 'test_helper'
-
-class CalendarDateTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
-end
--- test/unit/helpers/calendar_dates_helper_test.rb
+++
@@ -1,4 +0,0 @@
-require 'test_helper'
-
-class CalendarDatesHelperTest < ActionView::TestCase
-end
