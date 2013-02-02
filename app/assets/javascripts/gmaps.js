ca372ce485e940003ca3ffbe026197c901b40ff8
Alex Barry <alex.barry@gmail.com>
2013-02-02 13:33:53 -0500

    Simple index/root, working on getting gmaps to play nice

--- app/assets/javascripts/gmaps.js
+++ app/assets/javascripts/gmaps.js
@@ -6,12 +6,15 @@
   $(document).ready(function(){
     navigator.geolocation.watchPosition(
       function(pos){
+        console.log( pos );
         if ( !map ) {
           // pos.coords.latitude, pos.coords.longitude, pos.coords.accuracy, new Date(pos.timestamp)
           var mapOptions = {
-            center: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude), zoom: 5, mapTypeId: google.maps.MapTypeId.ROADMAP
+            center: new google.maps.LatLng(pos.coords.latitude, pos.coords.longitude),
+            zoom: 5,
+            mapTypeId: google.maps.MapTypeId.ROADMAP
           };
-          var map = new google.maps.Map( $("#gmap_canvas"), mapOptions);
+          var map = new google.maps.Map( $("#gmap_canvas")[0], mapOptions);
         }
       }, function(e){
         console.log("GPS Exception", e);
