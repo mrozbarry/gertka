c33d975067291e8cc993974bd93cef30eb85199c
Stephen Paul Weber <singpolyma@singpolyma.net>
2013-02-02 12:03:39 -0500

    Cat get stops with busses today

--- app/models/stop.rb
+++ app/models/stop.rb
@@ -1,3 +1,15 @@
 class Stop < ActiveRecord::Base
   attr_accessible :location_type, :stop_desc, :stop_id, :stop_lat, :stop_lon, :stop_name, :zone_id
+
+  has_many :stop_times, :primary_key => :stop_id
+
+  scope :busses_today, lambda {
+    wday = [:sunday,:monday,:tuesday,:wednesday,:thursday,:friday,:saturday][Date.today.wday]
+
+    Stop.joins(:stop_times => {:trip => :calendar}).where(:calendars => {
+      wday => true,
+      :start_dat => (Time.at(0)..Time.now),
+      :end_date => (Time.now..Time.parse('2038-01-01')),
+    })
+  }
 end
--- app/models/stop_time.rb
+++ app/models/stop_time.rb
@@ -1,4 +1,5 @@
 class StopTime < ActiveRecord::Base
   attr_accessible :arrival_time, :departure_time, :dropoff_type, :pickup_type, :shape_dist_traveled, :stop_headsign, :stop_id, :stop_sequence, :trip_id
-  has_one :trip
+
+  belongs_to :trip, :primary_key => :trip_id, :foreign_key => :trip_id
 end
